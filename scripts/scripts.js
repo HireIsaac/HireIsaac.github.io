e8a0d17a4503a2f8038a82519b827510ee62b1e0
Isaac Lohnes <irlohnes3@outlook.com>
2018-11-24 14:16:44 -0400

    TODO: Mobile responsiveness, image galleries

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,4 +6,25 @@ $(document).ready(function () {
         },
             'slow');
     });
+
+    $("#nav2").click(function () {
+        $('html,body').animate({
+            scrollTop: $("#resume").offset().top
+        },
+            'slow');
+    });
+
+    $("#nav3").click(function () {
+        $('html,body').animate({
+            scrollTop: $("#education").offset().top
+        },
+            'slow');
+    });
+
+    $("#nav4").click(function () {
+        $('html,body').animate({
+            scrollTop: $("#contact").offset().top
+        },
+            'slow');
+    });
 });
